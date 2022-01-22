58e3295d8826f1258216db832037ab5afee3733a
Dave Poirier <dave@encoded.life>
2022-01-21 21:23:30 -0500

    Base package with IPAddressValidator

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # SwiftEmailValidator
-Proper email validation in Swift
+
+A Swift implementation of an international email address syntax validator.  Since email addresses are local @ remote
+the validator also includes IPAddressValidator and EmailHostValidator classes.
+
+## IPAddressValidator
+
+    if IPAddressValidator.isIPv6Address("::1") {
+        print("::1 is a valid IPv6 address")
+    }
+
+    if IPAddressValidator.isIPv4Address("127.0.0.1") {
+        print("127.0.0.1 is a valid IPv4 address")
+    }
+    
+    if IPAddressValidator.isIPAddress("8.8.8.8") {
+        print("8.8.8.8 is a valid IP address")
+    }
+    
+    if IPAddressValidator.isIPAddress("fe80::1") {
+        print("fe80::1 is a valid IP address")
+    }
+
---
+++ Tests/SwiftEmailValidatorTests/IPAddressValidatorTests.swift
@@ -0,0 +1,81 @@
+//
+//  IPAddressValidatorTests.swift
+//  SwiftEmailValidator
+//
+//  Created by Dave Poirier on 2022-01-21.
+//  Copyrights (C) 2022, Dave Poirier.  Distributed under MIT license
+
+import XCTest
+@testable import SwiftEmailValidator
+
+final class IPAddressValidatorTests: XCTestCase {
+
+    let validIPv6Addresses: [String] = [
+        "1:2:3:4:5:6:7:8",
+        "::ffff:10.0.0.1",
+        "::ffff:1.2.3.4",
+        "::ffff:0.0.0.0",
+        "1:2:3:4:5:6:77:88",
+        "::ffff:255.255.255.255",
+        "fe08::7:8",
+        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
+    ]
+    
+    let invalidIPv6Addresses: [String] = [
+        "1:2:3:4:5:6:7:8:9",
+        "1:2:3:4:5:6::7:8",
+        ":1:2:3:4:5:6:7:8",
+        "1:2:3:4:5:6:7:8:",
+        "::1:2:3:4:5:6:7:8",
+        "1:2:3:4:5:6:7:8::",
+        "1:2:3:4:5:6:7:88888",
+        "2001:db8:3:4:5::192.0.2.33",
+        "fe08::7:8%",
+        "fe08::7:8i",
+        "fe08::7:8interface"
+    ]
+    
+    let validIPv4Addresses: [String] = [
+        "0.0.0.0",
+        "9.9.9.9",
+        "99.99.99.99",
+        "199.199.199.199",
+        "200.200.200.200",
+        "255.255.255.255",
+        "192.168.2.1",
+        "10.0.3.57",
+        "172.16.9.255"
+    ]
+    
+    let invalidIPv4Addresses: [String] = [
+        "0.0.0",
+        "0.0.0.",
+        ".0.0.0",
+        ".0.0.0.0",
+        "0.0.0.0.",
+        "256.2.3.4",
+        "1.256.3.4",
+        "1.2.256.4",
+        "1.2.3.256",
+        "1000.2.3.4",
+        "300.2.3.4"
+    ]
+
+    func testValidIPv6Addresses() {
+        validIPv6Addresses.forEach { XCTAssertTrue(IPAddressValidator.isIPv6Address($0), "Expected \($0) to be a valid IPv6 address") }
+    }
+    
+    func testInvalidIPv6Addresses() {
+        invalidIPv6Addresses.forEach { XCTAssertFalse(IPAddressValidator.isIPv6Address($0), "Expected \($0) to be an invalid IPv6 address") }
+        validIPv4Addresses.forEach { XCTAssertFalse(IPAddressValidator.isIPv6Address($0), "Expected \($0) to be a valid IPv4 but not a valid IPv6 address") }
+    }
+    
+    func testValidIPv4Addresses() {
+        validIPv4Addresses.forEach { XCTAssertTrue(IPAddressValidator.isIPv4Address($0), "Expected \($0) to be a valid IPv4 address") }
+    }
+    
+    func testInvalidIPv4Addresses() {
+        invalidIPv4Addresses.forEach { XCTAssertFalse(IPAddressValidator.isIPv4Address($0), "Expected \($0) to be an invalid IPv4 address") }
+        validIPv6Addresses.forEach { XCTAssertFalse(IPAddressValidator.isIPv4Address($0), "Expected \($0) to be a valid IPv6 but not a valid IPv4 address") }
+    }
+}
